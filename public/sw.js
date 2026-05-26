d8522670ad91f736294eeaa6cac96db3e5dca577
fukreyy <fukreyy@github.com>
2026-05-26 11:59:19 +0530

    fix sw.js push notification handler

--- public/sw.js
+++ public/sw.js
@@ -3,3 +3,22 @@ self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
 ));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+
+self.addEventListener('push', e => {
+  let data = {};
+  try { data = e.data.json(); } catch (_) { data = { title: 'Fukrey', body: e.data ? e.data.text() : 'New update!' }; }
+  e.waitUntil(
+    self.registration.showNotification(data.title || 'Fukrey 🎬', {
+      body: data.body || '',
+      icon: data.icon || '/gang/icon.png',
+      badge: '/gang/icon.png',
+      data: data.data || {},
+      vibrate: [200, 100, 200],
+    })
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('https://fukreyy.github.io/gang/'));
+});
